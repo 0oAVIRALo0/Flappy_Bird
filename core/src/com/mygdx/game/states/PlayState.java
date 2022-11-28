57e0de3e868e092856132d0d91495fca03beffe3
ItachiUchiha30 <87933991+ItachiUchiha30@users.noreply.github.com>
2022-11-28 14:08:59 +0530

    final

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -3,18 +3,28 @@ package com.mygdx.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.sprites.Bird;
+import com.mygdx.game.sprites.Tube;
 
 public class PlayState extends State{
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
     private Bird bird;
     private Texture bg;
+    private Array<Tube> tubes;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 100);
         bg = new Texture("bg.png");
         camera.setToOrtho(false, MyGdxGame.WIDTH / 2f, MyGdxGame.HEIGHT / 2f);
+
+        tubes = new Array<Tube>();
+        for (int i = 1; i <= TUBE_COUNT; i++) {
+            tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
+        }
     }
 
     @Override
@@ -28,6 +38,14 @@ public class PlayState extends State{
     public void update(float delta) {
         handleInput();
         bird.update(delta);
+        camera.position.x = bird.getPosition().x + 80;
+
+        for (Tube tube : tubes) {
+            if (camera.position.x - (camera.viewportWidth/2f) > tube.getPosTopTube().x + tube.getTopTube().getWidth()) {
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+        }
+        camera.update();
     }
 
     @Override
@@ -36,6 +54,10 @@ public class PlayState extends State{
         batch.begin();
         batch.draw(bg, camera.position.x - (camera.viewportWidth/2f), 0);
         batch.draw(bird.getBird(), bird.getPosition().x, bird.getPosition().y);
+        for (Tube tube : tubes) {
+            batch.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+            batch.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        }
         batch.end();
     }
 
